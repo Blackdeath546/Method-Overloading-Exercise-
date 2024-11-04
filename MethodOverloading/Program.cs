160cfa904b0a49fdfceb387b65319b2b2bf92d68
Nicholas Angeles <blackdeath546@gmail.com>
2024-11-03 22:04:19 -0500

    Overloading crazy methods with more methods

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,50 @@
 {
     public class Program
     {
+
+        static void Add(int a, int b)
+        {
+            int sum = a + b; 
+            Console.WriteLine(a + b);
+        }
+        
+        static void Add(double a, double b)
+        {
+            double sum = a + b; 
+            Console.WriteLine(a + b);
+        }
+        
+        static void Add(double a, double b, string c, bool d)
+        {
+            var sum = a + b;
+            if (sum == 1 || sum == -1)
+            {
+                c = "dollar";
+            }
+            else if (sum > 1 || sum == 0 || sum < -1)
+            {
+                c = "dollars";
+            }
+            else if (sum < 1 && sum > 0 || sum < -1 && sum > 0)
+            {
+                c = "cents";
+            }
+           
+            if (d == true)
+            { 
+                Console.WriteLine($"{a + b} {c}");
+                
+            }
+            else
+            {
+                Console.WriteLine($"{a + b}");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(2, 3);
+            Add(2.5, 2.6);
+            Add(5, 6, "dollars", false);
         }
     }
 }
